f3dd7c1a407e1ca80e07e6bf0bf5c8c042073b72
Gabriel145-js <gabrielzapa456@gmail.com>
2025-07-14 18:43:37 -0300

    feat: rotas novas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.sass'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 const App = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <About/>
+    <Skills/>
     </>
   );
 };
