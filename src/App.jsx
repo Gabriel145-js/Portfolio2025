1429012df9528ad53ef0e026c604f0b7309c18ae
Gabriel145-js <gabrielzapa456@gmail.com>
2025-07-15 16:57:45 -0300

    feat: nova rota de projetos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Projetos from './components/Projetos/Projetos';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
     <Hero/>
     <About/>
     <Skills/>
+    <Projetos/>
     </>
   );
 };
