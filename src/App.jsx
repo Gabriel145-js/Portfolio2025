356070deb68fb33213c52f0b74c07c3accfc60bd
Gabriel145-js <gabrielzapa456@gmail.com>
2025-07-18 10:46:05 -0300

    fix: ajuste para o useInViwe funcionar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import { Element } from 'react-scroll'
 import './App.sass'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projetos from './components/Projetos/Projetos';
+import Contatos from './components/Contatos/Contatos';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <About/>
-    <Skills/>
-    <Projetos/>
+      <Navbar />
+
+      <Element id="inicio" className="section conteudo">
+        <Hero />
+      </Element>
+
+      <Element id="about" className="section">
+        <About />
+      </Element>
+
+      <Element id="skills" className="section">
+        <Skills />
+      </Element>
+
+      <Element id="projetos" className="section">
+        <Projetos />
+      </Element>
+
+      <Element id="contatos" className="section">
+        <Contatos />
+      </Element>
+
+      <Footer />
     </>
   );
 };
