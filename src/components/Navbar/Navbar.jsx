44cba7fb06519f042305b58c99ed51339e9607a3
Gabriel145-js <gabrielzapa456@gmail.com>
2025-07-18 14:25:48 -0300

    feat: ajuste para menuMobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-scroll'
 import styles from './Navbar.module.sass'
+import { ReactComponent as IconMenu } from '../../icons/menuHamburguer.svg'
 
 const Navbar = () => {
     const [showOverlay, setShowOverlay] = useState(false)
     const [activeSection, setActiveSection] = useState('inicio')
+    const [isMobile, setIsMobile] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isAnimatingClose, setIsAnimatingClose] = useState(false)
 
     useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 900)
+            if (window.innerWidth > 900) {
+                setIsMenuOpen(false)
+            }
+        }
+
         const handleScroll = () => {
             const scrollTop = window.scrollY
             setShowOverlay(scrollTop > 50)
@@ -25,10 +36,10 @@ const Navbar = () => {
                     const rect = element.getBoundingClientRect()
                     const elementTop = rect.top + window.scrollY
                     const elementCenter = elementTop + (rect.height / 2)
-                    
+
                     // Calcula a distância entre o centro da tela e o centro da seção
                     const distance = Math.abs(scrollPosition - elementCenter)
-                    
+
                     // A seção mais próxima do centro da tela é a ativa
                     if (distance < minDistance) {
                         minDistance = distance
@@ -41,10 +52,33 @@ const Navbar = () => {
         }
 
         window.addEventListener('scroll', handleScroll)
+        window.addEventListener('resize', handleResize)
+
         handleScroll() // Chama uma vez para definir o estado inicial
-        
-        return () => window.removeEventListener('scroll', handleScroll)
+        handleResize() // Chama uma vez para definir o estado inicial
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('resize', handleResize)
+        }
     }, [])
+
+    const toggleMenu = () => {
+        if (isMenuOpen) {
+            setIsAnimatingClose(true)
+            setTimeout(() => {
+                setIsMenuOpen(false)
+                setIsAnimatingClose(false)
+            }, 300) // tempo da animação de fechamento
+        } else {
+            setIsMenuOpen(true)
+        }
+    }
+
+    const handleLinkClick = () => {
+        toggleMenu()
+    }
+
 
     return (
         <div className={styles.container}>
@@ -54,53 +88,114 @@ const Navbar = () => {
                 <div className={styles.logoNavbar}>
                     <h3>DEV<span>GABRIEL</span></h3>
                 </div>
-                <div className={styles.navLinks}>
-                    <Link 
-                        to="inicio" 
-                        smooth={true} 
-                        duration={150} 
-                        offset={-80}
-                        className={activeSection === 'inicio' ? styles.active : ''}
-                    >
-                        Inicio
-                    </Link>
-                    <Link 
-                        to="about" 
-                        smooth={true} 
-                        duration={150} 
-                        offset={-80}
-                        className={activeSection === 'about' ? styles.active : ''}
-                    >
-                        Sobre
-                    </Link>
-                    <Link 
-                        to="skills" 
-                        smooth={true} 
-                        duration={150} 
-                        offset={-120}
-                        className={activeSection === 'skills' ? styles.active : ''}
-                    >
-                        Habilidades
-                    </Link>
-                    <Link 
-                        to="projetos" 
-                        smooth={true} 
-                        duration={150} 
-                        offset={-120}
-                        className={activeSection === 'projetos' ? styles.active : ''}
-                    >
-                        Projetos
-                    </Link>
-                    <Link 
-                        to="contatos" 
-                        smooth={true} 
-                        duration={150} 
-                        offset={-80}
-                        className={activeSection === 'contatos' ? styles.active : ''}
-                    >
-                        Contatos
-                    </Link>
-                </div>
+
+                {isMobile ? (
+                    <>
+                        <div className={styles.hamburgerIcon} onClick={toggleMenu}>
+                            <IconMenu />
+                        </div>
+                        <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.mobileMenuOpen : ''}`}>
+                            <Link
+                                to="inicio"
+                                smooth={true}
+                                duration={150}
+                                offset={-80}
+                                className={activeSection === 'inicio' ? styles.active : ''}
+                                onClick={handleLinkClick}
+                            >
+                                Inicio
+                            </Link>
+                            <Link
+                                to="about"
+                                smooth={true}
+                                duration={150}
+                                offset={-80}
+                                className={activeSection === 'about' ? styles.active : ''}
+                                onClick={handleLinkClick}
+                            >
+                                Sobre
+                            </Link>
+                            <Link
+                                to="skills"
+                                smooth={true}
+                                duration={150}
+                                offset={-120}
+                                className={activeSection === 'skills' ? styles.active : ''}
+                                onClick={handleLinkClick}
+                            >
+                                Habilidades
+                            </Link>
+                            <Link
+                                to="projetos"
+                                smooth={true}
+                                duration={150}
+                                offset={-120}
+                                className={activeSection === 'projetos' ? styles.active : ''}
+                                onClick={handleLinkClick}
+                            >
+                                Projetos
+                            </Link>
+                            <Link
+                                to="contatos"
+                                smooth={true}
+                                duration={150}
+                                offset={-80}
+                                className={activeSection === 'contatos' ? styles.active : ''}
+                                onClick={handleLinkClick}
+                            >
+                                Contatos
+                            </Link>
+                        </div>
+                    </>
+                ) : (
+                    <div className={styles.navLinks}>
+                        <Link
+                            to="inicio"
+                            smooth={true}
+                            duration={150}
+                            offset={-80}
+                            className={activeSection === 'inicio' ? styles.active : ''}
+                        >
+                            Inicio
+                        </Link>
+                        <Link
+                            to="about"
+                            smooth={true}
+                            duration={150}
+                            offset={-80}
+                            className={activeSection === 'about' ? styles.active : ''}
+                        >
+                            Sobre
+                        </Link>
+                        <Link
+                            to="skills"
+                            smooth={true}
+                            duration={150}
+                            offset={-120}
+                            className={activeSection === 'skills' ? styles.active : ''}
+                        >
+                            Habilidades
+                        </Link>
+                        <Link
+                            to="projetos"
+                            smooth={true}
+                            duration={150}
+                            offset={-120}
+                            className={activeSection === 'projetos' ? styles.active : ''}
+                        >
+                            Projetos
+                        </Link>
+                        <Link
+                            to="contatos"
+                            smooth={true}
+                            duration={150}
+                            offset={-80}
+                            className={activeSection === 'contatos' ? styles.active : ''}
+                        >
+                            Contatos
+                        </Link>
+                    </div>
+                )}
             </nav>
         </div>
     )
