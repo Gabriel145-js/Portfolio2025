baf57d69ed820494f709c7f30071205f43dea3f8
Gabriel145-js <gabrielzapa456@gmail.com>
2025-07-18 10:46:54 -0300

    feat: ajuste para useInViwe funcionar

---
+++ src/components/Contatos/ContactForm.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import styles from './Contatos.module.sass'
+import useInView from '../../hooks/useInView';
+
+
+function ContactForm() {
+  const [ref, isInView] = useInView({ threshold: 0.3 })
+  const [state, handleSubmit] = useForm("xyzpybga")
+
+  if (state.succeeded) {
+    return <p>Obrigado pela mensagem!</p>;
+  }
+
+  // ...
+
+  return (
+    <form ref={ref} onSubmit={handleSubmit} className={`${styles.formContact} ${isInView ? styles.slideLeft : ''}`} >
+      {/* Linha com Nome e Email */}
+      <div className={styles.rowInputs}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="name">Nome</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            required
+            placeholder="Seu nome"
+          />
+          <ValidationError
+            prefix="Nome"
+            field="name"
+            errors={state.errors}
+          />
+        </div>
+
+        <div className={styles.inputWrapper}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            placeholder="seu@email.com"
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+        </div>
+      </div>
+
+
+      {/* Assunto */}
+      <label htmlFor="subject">Assunto</label>
+      <input
+        id="subject"
+        type="text"
+        name="subject"
+        placeholder='Qual o seu assunto?'
+        required
+      />
+      <ValidationError
+        prefix="Assunto"
+        field="subject"
+        errors={state.errors}
+      />
+
+      {/* Mensagem */}
+      <label htmlFor="message">Mensagem</label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder='Sua mensagem...'
+        required
+      />
+      <ValidationError
+        prefix="Mensagem"
+        field="message"
+        errors={state.errors}
+      />
+
+      <button type="submit" disabled={state.submitting}>
+        Enviar
+      </button>
+    </form>
+  );
+
+}
+
+export default ContactForm;
---
+++ src/components/Contatos/Contatos.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styles from './Contatos.module.sass';
+import ContactForm from './ContactForm';
+import { ReactComponent as IconLocation } from '../../icons/location.svg';
+import { ReactComponent as IconEmail } from '../../icons/email.svg';
+import { ReactComponent as IconFone } from '../../icons/fone.svg';
+import { ReactComponent as IconWpp } from '../../icons/redesSociais/redesWppIcon.svg'
+import { ReactComponent as IconLinkedin } from '../../icons/redesSociais/redesLinkeIcon.svg'
+import { ReactComponent as IconGitHub } from '../../icons/redesSociais/redesGitHub.svg'
+import useInView from '../../hooks/useInView';
+
+
+const Contatos = () => {
+     const [ref, isInView] = useInView({ threshold: 0.3 })
+
+    return (
+        <div ref={ref} className={styles.containerContatos}>
+            <div className={`${styles.tituloSubtitulo} ${isInView ? styles.tracking : ''}`}>
+                <h2>Entre em <span>Contato</span></h2>
+                <p>Entre em contato comigo utilizando o formulário ou pelas informações ao lado.</p>
+            </div>
+
+            <section className={styles.sectionCtts}>
+                <div className={styles.containerForm}>
+                    <ContactForm />
+                </div>
+
+                <div className={`${styles.cardCtts} ${isInView ? styles.slideRight : ''}`}>
+                    <h3>Informações de Contato</h3>
+
+                    <div className={styles.itemInfo}>
+                        <IconLocation />
+                        <div>
+                            <p>Localização</p>
+                            <span>Caxias do Sul - RS, Brasil</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.itemInfo}>
+                        <IconEmail />
+                        <div>
+                            <p>Email</p>
+                            <a href="mailto:gabriel.m.zaparolli@gmail.com">gabriel.m.zaparolli@gmail.com</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.itemInfo}>
+                        <IconFone />
+                        <div>
+                            <p>Telefone</p>
+                            <a href="tel:+54991529657">(54) 99152-9657</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.redesSociais}>
+                        <h3>Redes Sociais</h3>
+                        <div className={styles.iconesRedes}>
+                           <a href="" className={styles.wppLink}><IconWpp/></a>
+                           <a href="" className={styles.linkedinLink}><IconLinkedin/></a>
+                           <a href="" className={styles.githubLink}><IconGitHub/></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Contatos;
